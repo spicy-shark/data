6f984af5ac09881e5aeea086121cb5440dc41ec9
PeiYansheng <peiyansheng@shanda.com>
2022-05-04 15:05:36 +0800

    hot100-4菜鸡提交

--- Simple/src/top100/FindMedianSortedArrays.java
+++ Simple/src/top100/FindMedianSortedArrays.java
@@ -22,10 +22,14 @@ public class FindMedianSortedArrays {
     int len1 = end1 - start1 + 1;
     int len2 = end2 - start2 + 1;
     //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
-    if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+    if (len1 > len2) {
+      return getKth(nums2, start2, end2, nums1, start1, end1, k);
+    }
     if (len1 == 0) return nums2[start2 + k - 1];
 
-    if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+    if (k == 1) {
+      return Math.min(nums1[start1], nums2[start2]);
+    }
 
     int i = start1 + Math.min(len1, k / 2) - 1;
     int j = start2 + Math.min(len2, k / 2) - 1;
